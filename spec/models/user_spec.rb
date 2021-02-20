c9e04914358dbc5431f6575acccaff6bb6f134f0
angelica-thd <ageliki.th@gmail.com>
2021-02-20 13:59:23 +0200

    configure _spec and factory bots for each model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # ensure User model has a 1:m relationship with the Student model
+  it { should have_many(:students) }
+  # Validation tests
+  # ensure name, email and password_digest are present before save
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
 end
